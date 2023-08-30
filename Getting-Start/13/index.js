971d9dee3325925ba221bd2998f7968202c9ef40
Muhammad-Ali-bhatti <al.khan1001@gmail.com>
2023-08-31 01:11:51 +0500

    Modified Array Playing With Array

--- Getting-Start/13/index.js
+++ Getting-Start/13/index.js
@@ -1,6 +1,13 @@
 "use strict";
-let guest_list = ['Muhammad Ali', 'Muhammad Rasheed', 'Usman'];
+let guest_list = ['Muhammad Ali', 'Muhammad Rasheed', 'Malik Usman', "Zaki ALi"];
 console.log("Hey! EveryOne We Found Bigger Dinning Table");
-guest_list.shift();
+// First Element
 guest_list.unshift("Muhammad Usman");
-console.log(guest_list);
+// Middle Element
+guest_list.splice(2, 0, "Iman Ishtiaq");
+//Add Element In The End Of Your List
+guest_list.push("Abdul Rasheed");
+// New Set Of Invitation
+for (let i = 0; i < guest_list.length; i++) {
+    console.log("Congrulation You,re Invited", guest_list[i]);
+}
--- Getting-Start/13/index.ts
+++ Getting-Start/13/index.ts
@@ -4,9 +4,18 @@ let guest_list = ['Muhammad Ali', 'Muhammad Rasheed', 'Malik Usman', "Zaki ALi"]
 
 console.log("Hey! EveryOne We Found Bigger Dinning Table");
 // First Element
-guest_list.shift();
+
 guest_list.unshift("Muhammad Usman");
 // Middle Element
+guest_list.splice(2, 0, "Iman Ishtiaq");
+
+//Add Element In The End Of Your List
+guest_list.push("Abdul Rasheed");
 
 
-console.log(guest_list)
+// New Set Of Invitation
+
+for (let i = 0; i < guest_list.length; i++)
+{
+    console.log("Congrulation You,re Invited", guest_list[i] );
+}
